d21b284da7252dd4a37db0753cc188db719e4af4
Rashik <rashikraihan8@gmail.com>
2026-02-13 18:49:33 +0600

    Modulo

--- src/problem1.h
+++
@@ -1,8 +0,0 @@
-#ifndef problem1_H
-#define problem1_H
-
-void printProblem1Details(int path[], int pathLen, int source, int target, 
-                          double srcLat, double srcLon, double destLat, double destLon);
-void runProblem1();
-
-#endif
